c0fcaa2b52065e8fe2835dd0009be28c1b768e40
cd <azzamathar@hotmail.com>
2019-07-30 20:48:14 +0000

    created an interactive timer

---
+++ timer2.js
@@ -0,0 +1,21 @@
+const keyArr = ['1','2','3','4','5','6','7','8','9'];
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+stdin.on('data', (key) => {
+  if (key === '\u0062') {
+    process.stdout.write('\x07');
+  } else if (key === '\u0003') {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  } else if (keyArr.includes(key)) {
+    console.log(`Setting timer for ${key} seconds...`);
+    setTimeout(()=> {
+      process.stdout.write('\x07');
+    },key * 1000);
+  } else {
+    return;
+  }
+});
+console.log('after callback');
